3830a2c819915e89209b34473d74e403e9d97125
Brandon Chin <brandonchin@me.com>
2018-03-01 04:28:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     @this_dogs_name = dog_name
   end
 
-  def name
+  def name_of_dog
     @this_dogs_name
   end
 end
@@ -11,4 +11,4 @@ end
 lassie = Dog.new
 lassie.name = "Lassie"
 
-puts lassie.name
+puts lassie.name_of_dog
